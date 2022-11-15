93a9ec36832c26ae7e3893d5bbb4620c0dad642c
Eunhyung Choi <milkiss0414@naver.com>
2022-11-16 00:11:25 +0900

    Update: 폰트 적용

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -5,9 +5,20 @@ import theme from "./theme";
 const GlobalStyle = createGlobalStyle`
     ${reset}
     // 아래에 전역 스타일 추가
+    @font-face {
+        font-family: 'NeoDunggeunmo';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.3/NeoDunggeunmo.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+
     html {
-        font-family: 'Apple Sandoll Gothic Neo';
         background-color: ${theme.colors.bg};
+        color: white;
+        font-family: 'NeoDunggeunmo';
+    }
+
+    h1 {
     }
 `;
 
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -2,9 +2,34 @@ const colors = {
   bg: "#1D213A",
 } as const;
 
+interface Font {
+  weight: 300 | 400 | 500 | 600 | 700;
+  size: number;
+  lineHeight: number;
+  letterSpacing: number;
+}
+
+function FONT({ weight, size, lineHeight, letterSpacing }: Font): string {
+  return `
+      font-weight: ${weight};
+      font-size: ${size}px;
+      line-height: ${lineHeight}px;
+      letter-spacing: -0.0${letterSpacing}px;
+    `;
+}
+
 const fonts = {
-  mainFonts: "DungGeunMo",
-};
+  title1: FONT({ weight: 700, size: 20, lineHeight: 20, letterSpacing: 5 }),
+  title2: FONT({ weight: 700, size: 18, lineHeight: 20, letterSpacing: 5 }),
+  title3: FONT({ weight: 700, size: 17, lineHeight: 20, letterSpacing: 5 }),
+  subtitle1: FONT({ weight: 700, size: 16, lineHeight: 20, letterSpacing: 5 }),
+  subtitle2: FONT({ weight: 600, size: 16, lineHeight: 20, letterSpacing: 5 }),
+  body1: FONT({ weight: 500, size: 16, lineHeight: 20, letterSpacing: 5 }),
+  body2: FONT({ weight: 400, size: 16, lineHeight: 20, letterSpacing: 5 }),
+  body3: FONT({ weight: 600, size: 15, lineHeight: 20, letterSpacing: 5 }),
+  caption1: FONT({ weight: 600, size: 14, lineHeight: 20, letterSpacing: 5 }),
+  caption2: FONT({ weight: 500, size: 14, lineHeight: 20, letterSpacing: 5 }),
+} as const;
 
 const theme = {
   colors,
